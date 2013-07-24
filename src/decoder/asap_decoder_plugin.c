bd01a52759f57471adac687b2222d403419081a4
Yuxuan Shui <yshuiv7@gmail.com>
2013-07-24 16:24:27 +0800

    decoder, asap: Add a copyright header

--- src/decoder/asap_decoder_plugin.c
+++ src/decoder/asap_decoder_plugin.c
@@ -1,3 +1,9 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* Copyright (C) 2013 Yuxuan Shui, yshuiv7@gmail.com */
+
 #include "config.h"
 #include "../decoder_api.h"
 #include "audio_check.h"
